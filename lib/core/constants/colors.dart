2878aa2228e37fe8c02f8815885924696389071f
Imdadul Haque <imd1234566@gmail.com>
2025-02-10 21:15:37 +0530

    Added white color

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const appBgColor = Color(0xff1B262C);
-const onPrimary = Color(0xff222E34);
+const kbgcolor = Color(0xff1B262C);
+const kprimary = Color(0xff222E34);
+const kwhite = Color(0xffffffff);
 
 const kyellow = Color.fromARGB(255, 245, 154, 18);
