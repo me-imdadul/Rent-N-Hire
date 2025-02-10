2d94905ce921a5bc02df160adff4bac61d0b4c6e
Imdadul Haque <imd1234566@gmail.com>
2025-02-10 21:17:55 +0530

    Added search field in search page

--- lib/features/search/presentation/screens/search.dart
+++ lib/features/search/presentation/screens/search.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:iconly/iconly.dart';
+import 'package:rent_n_hire/core/constants/colors.dart';
+import 'package:rent_n_hire/core/constants/sizes.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -6,9 +11,47 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Search'),
+      appBar: AppBar(
+        toolbarHeight: kToolbarHeight + 20,
+        title: Container(
+          padding: const EdgeInsets.all(3),
+          decoration: BoxDecoration(
+              color: kwhite,
+              boxShadow: [
+                BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 10)
+              ],
+              borderRadius: BorderRadius.circular(AppSizes.cardradius)),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    IconlyLight.arrow_left_2,
+                    color: kyellow,
+                    size: AppSizes.iconsize,
+                  )),
+              const Gap(10),
+              Expanded(
+                  child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    hintText: "Search here",
+                    hintStyle:
+                        TextStyle(fontSize: 15, color: Colors.grey.shade500)),
+              )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    IconlyLight.filter,
+                    color: kyellow,
+                    size: AppSizes.iconsize,
+                  ))
+            ],
+          ),
+        ),
       ),
+      body: const Center(child: Text("sdsdr")),
     );
   }
 }
